db7a01429ac3690ffbc7f59f741d7b3eff7f652d
tam-creater <tamshinj@fsi.co.jp>
2023-06-21 12:49:40 +0900

    subに追加2

--- app/src/main/java/com/example/attendance/AttendanceConfirmationActivity.kt
+++ app/src/main/java/com/example/attendance/AttendanceConfirmationActivity.kt
@@ -7,11 +7,28 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class AttendanceConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //データベースからデータの受け取り
+        val dataset: ArrayList<String> = arrayListOf(
+            "奥山 博文",
+            "小林 大樹",
+            "与儀 武志",
+            "田村 真司",
+            "箕輪 琢磨",
+            "六原 大地")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_attendance_confirmation)
+
+        val recyclerView: RecyclerView = findViewById(R.id.my_recycler_view)
+        recyclerView.setHasFixedSize(true)
+
+        val rLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = rLayoutManager
 
         //ユーザー名の取得
         val selectedName = intent.getStringExtra("NAME")
@@ -20,7 +37,7 @@ class AttendanceConfirmationActivity : AppCompatActivity() {
         val selectedTime = intent.getStringExtra("TIME")
 
         //選択されたユーザーの色変更
-        val selectedBtn: Button = when(selectedName) {
+        /*val selectedBtn: Button = when(selectedName) {
             "奥山 博文" -> findViewById(R.id.name1)
             "小林 大樹" -> findViewById(R.id.name2)
             "与儀 武志" -> findViewById(R.id.name3)
@@ -30,10 +47,10 @@ class AttendanceConfirmationActivity : AppCompatActivity() {
             else -> findViewById(R.id.name1)
         }
 
-        selectedBtn.setTextColor(Color.BLUE)
+        selectedBtn.setTextColor(Color.BLUE)*/
 
         //ステータスの変更
-        val selectedText: TextView = when(selectedName) {
+        /*val selectedText: TextView = when(selectedName) {
             "奥山 博文" -> findViewById(R.id.status1)
             "小林 大樹" -> findViewById(R.id.status2)
             "与儀 武志" -> findViewById(R.id.status3)
@@ -43,13 +60,13 @@ class AttendanceConfirmationActivity : AppCompatActivity() {
             else -> findViewById(R.id.status1)
         }
 
-        selectedText.text = selectedStatus
+        selectedText.text = selectedStatus*/
 
         //履歴に送るフォーマット
         val history = "$selectedYear $selectedTime $selectedStatus"
 
         //画面遷移
-        val btn1: Button = findViewById(R.id.name1)
+        /*val btn1: Button = findViewById(R.id.name1)
         val btn2: Button = findViewById(R.id.name2)
         val btn3: Button = findViewById(R.id.name3)
         val btn4: Button = findViewById(R.id.name4)
@@ -96,6 +113,6 @@ class AttendanceConfirmationActivity : AppCompatActivity() {
             intent.putExtra("NAME", selectedName)
             intent.putExtra("TIME", history)
             startActivity(intent)
-        }
+        }*/
     }
 }
